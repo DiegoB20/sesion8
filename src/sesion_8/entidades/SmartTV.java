751bba81f0e3b38eb6cf344bc6571a41a0af1eb0
DiegoB20 <becerragerardo16@gmail.com>
2024-05-19 11:30:15 -0500

    Add files via upload

---
+++ src/sesion_8/entidades/SmartTV.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sesion_8.entidades;
+
+import sesion_8.interfaces.navegable;
+import sesion_8.interfaces.reproducible;
+
+/**
+ *
+ * @author DIEGO
+ */
+public class SmartTV 
+       implements reproducible, navegable
+{
+
+    @Override
+    public void reproducir() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void navegar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/sesion_8/interfaces/camara.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package sesion_8.interfaces;
+
+/**
+ *
+ * @author DIEGO
+ */
+public interface camara {
+    
+    public void tomarFoto();
+    
+}
